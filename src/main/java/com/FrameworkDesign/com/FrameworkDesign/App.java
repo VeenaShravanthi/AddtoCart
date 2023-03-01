d8514c641d3c403e13a62ec6b9261f7966bd006f
veena <shravanthi.gvs@gmail.com>
2023-03-01 13:47:19 -0600

    First commit

---
+++ src/main/java/com/FrameworkDesign/com/FrameworkDesign/App.java
@@ -0,0 +1,13 @@
+package com.FrameworkDesign.com.FrameworkDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
